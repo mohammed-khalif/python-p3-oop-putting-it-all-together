438fc5b0a6a899c6cd6e6484ea1104fc911cf3b6
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-05-22 10:50:46 -0400

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,42 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("The shoe has been repaired.")
+        self.condition = "New"
